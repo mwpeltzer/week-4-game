16aaa12cf6b866069af39f314a45dee99569a163
Michael Peltzer <michaelpeltzer@Michaels-MacBook-Air.local>
2017-09-13 18:06:29 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # week-4-game
+
+Initial Pseudocode
+
+Cystal Guessing Game
+
+1.  Create wire frame for the separate sections of the game -
+
+	a) giant container that holds all of the separate divs inside
+	b) header div that contains the name of the game 'Cryatals Collecto'
+	c) Instructions div
+	d) div with computers random target number
+	e) right next to the 'd' div, div with wins/losses totals (plus an alert saying whether you won or lost that specific round)
+	f) div with with 4 seperate divs inside with 4 pictures of diamonds inside 
+	g) div with the text specifying your total score
+	h) div containing your current score
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,50 @@
 
-// 1. create code so that the computer generates a random number 
-// at the once the page is loaded
+
+	// Creates a user score variable that starts at zero
+	var userScore = 0;
+	var wins = 0;
+	var losses = 0;
+
+	// This creates a random number between 19 and 120
+	var computerRandom = getRandomInt(19, 120);
+	console.log(computerRandom);
+
+	// This creates a random number between 1 and 12 - will be used in each picture div
+	var userRandom = getRandomInt(1, 12);
+	console.log(userRandom);
+
+	// This is a random integer generator between 2 values
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max-min)) + min;
+		conosle.log(getRandomInt);
+	}
+
+	// Inputs the computers guess to the page 
+	$("#computer-random").html(computerRandom);
+
+	// Makes the image divs clickable
+	$("#crystal-div").on("click", function() {
+		alert("you clicked the crystal");
+	});
+
+	// Creates an array of the class gem
+	var gems = $(".gem");
+	
+	for (var i=0; i < gems.length; i++) {
+		$(gems[i]).attr("data-crystalvalue", userRandom(min, max));
+		
+	}
+	
+	// This is as far as I got, so I just did pseudocode for the remainder
+
+
+// 	  Make the crystal variable clickable so that each time it is clicked,
+//    the value is doubled (from whatever integer is selected randomly at the start) 
+
+// 	  create an 'if' statement that states if the user matches the computerRandom, an
+//    html appears saying you won and the tally in the wins column goes up by 1
+
+//    if not, "you lost" appers and the loss tally increases by 1
+
+//    at the end of both of these conditionals, the game should restart
+
